b1e1d9a3a7634bd81a0a3ee741f6404d61505309
J.A. Simmons V <simmonsj@jasimmonsv.com>
2012-07-20 18:45:43 -0500

    moves duplicate PDFs

--- duplicatePDFFinder.py
+++ duplicatePDFFinder.py
@@ -28,36 +28,40 @@ def copyPDF(root, e, destFolder):
 print "Finding PDFs...",
 startPath = os.getcwd()
 #Create needed directories
+samplePDFDir = '../sPDF'
+samplePDFPurDir = '../sPDFPurified'
 try:
-    os.mkdir('./sPDF')
+    os.mkdir(samplePDFDir)
 except OSError:
     sys.stderr.write('Directory exists')
 finally:    
-    destFolder1 = os.path.join(startPath,'./sPDF')
+    destFolder1 = os.path.join(startPath,samplePDFDir)
 try:
-    os.mkdir('./sPDFPurified')
+    os.mkdir(samplePDFPurDir)
 except OSError:
     sys.stderr.write('Directory exists')
 finally:    
-    destFolder2 = startPath+'./sPDFPurified'
+    destFolder2 = os.path.join(startPath,samplePDFPurDir)
+
 #Start the search for pdf's
 files1 = []
 root1 = []
 for root, dirs, files in os.walk(startPath):
     for e in files:
         if e.find('.pdf')>0:
-            if copyRandPDF(root, e):
+            if copyRandPDF(root, e, destFolder1):
                 root1.append(root)
                 files1.append(e)                
 #start the search for duplicates
-print 'Copying duplicates'
+print '\nCopying duplicates'
+#os.chdir('../PDFPurified')
 os.chdir('../PDFPurified')
 files2 = []
 root2 = []
 for root, dirs, files in os.walk(os.getcwd()):
     for e in files:
         if e in files1:
-            copyPDF(root,e)
+            copyPDF(root,e, destFolder2)
 
     
     
